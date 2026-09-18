a9c6cb8c74a427ed81d8ed07458f4d203f48019f
Matthew Dunford <128043740+MatthewDunford@users.noreply.github.com>
2026-09-18 09:12:33 +0100

    Delete a very time consuming bin pile directory

--- a very time consuming bin pile/sql commands/member in what team.sql
+++
@@ -1,9 +0,0 @@
-USE booking_system;
-
-SELECT 
-    member.name AS member_name, 
-    team.name AS team_name
-FROM member
-INNER JOIN team_has_member ON member.member_id = team_has_member.member_id
-INNER JOIN team ON team_has_member.team_id = team.team_id
-ORDER BY member.name ASC;
